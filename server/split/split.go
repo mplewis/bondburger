420d2454a3de01a8ae4cdca8150ace486d1913aa
Matt Lewis <matt@mplewis.com>
2021-09-22 22:57:47 -0600

    feat: no empty sentences, obey abbreviations

--- server/split/split.go
+++ server/split/split.go
@@ -5,19 +5,47 @@ import (
 	"strings"
 )
 
-func SplitSentences(all string) []string {
+// https://github.com/mediacloud/sentence-splitter/blob/9af995f451a193898953dfe3926d1beefb45afb6/sentence_splitter/non_breaking_prefixes/en.txt
+var abbrs = []string{
+	"a", "adj", "adm", "adv", "al", "apr", "art", "asst", "aug", "b", "bart", "bldg", "brig", "bros", "c", "capt", "cf",
+	"cmdr", "co", "col", "comdr", "con", "corp", "cpl", "d", "dec", "dr", "drs", "e", "e.g", "ens", "esp", "etc", "f",
+	"feb", "fig", "g", "gen", "gov", "h", "hon", "hosp", "hr", "i", "i.e", "inc", "insp", "j", "jan", "jr", "jul", "jun",
+	"k", "l", "lt", "m", "maj", "mar", "messrs", "mlle", "mm", "mme", "mr", "mrs", "ms", "msgr", "n", "no", "nos", "nov",
+	"nr", "o", "oct", "okt", "op", "ord", "p", "pfc", "ph", "ph.d", "phd", "pp", "prof", "pvt", "q", "r", "rep", "reps",
+	"res", "rev", "rt", "s", "sen", "sens", "sep", "sept", "sfc", "sgt", "sr", "st", "supt", "surg", "t", "u",
+	"u.k", "u.s", "v", "vs", "w", "x", "y", "z",
+}
+
+func Sentences(all string) []string {
 	all = strings.TrimSpace(all)
 	results := regexp.MustCompile(`([^\s]+)\.\s+`).FindAllStringIndex(all, -1)
 	sentences := make([]string, len(results))
 	lastPos := 0
+nextWord:
 	for i := 0; i < len(results); i++ {
+		lastRaw := all[results[i][0]:results[i][1]]
+		lastWord := strings.ToLower(strings.TrimSpace(lastRaw))
+
+		for _, abbr := range abbrs {
+			abbr := abbr + "."
+			if lastWord == abbr {
+				continue nextWord
+			}
+		}
+
 		currPos := results[i][1]
 		sentences[i] = all[lastPos:currPos]
 		lastPos = currPos
 	}
 	sentences = append(sentences, all[lastPos:])
-	for i, s := range sentences {
-		sentences[i] = strings.TrimSpace(s)
+
+	allTrimmed := []string{}
+	for _, s := range sentences {
+		trimmed := strings.TrimSpace(s)
+		if trimmed == "" {
+			continue
+		}
+		allTrimmed = append(allTrimmed, trimmed)
 	}
-	return sentences
+	return allTrimmed
 }
--- server/split/split_test.go
+++ server/split/split_test.go
@@ -16,19 +16,24 @@ func TestSplit(t *testing.T) {
 
 var _ = Describe("Split", func() {
 	It("splits paragraphs into sentences", func() {
-		body := "Foo. Bar.   Baz. Quux. Plugh."
-		Expect(split.SplitSentences(body)).To(Equal([]string{"Foo.", "Bar.", "Baz.", "Quux.", "Plugh."}))
+		body := "Foo. Bar. Baz. Quux. Plugh."
+		Expect(split.Sentences(body)).To(Equal([]string{"Foo.", "Bar.", "Baz.", "Quux.", "Plugh."}))
+		body = "   Foo. Bar.   Baz. Quux. Plugh. "
+		Expect(split.Sentences(body)).To(Equal([]string{"Foo.", "Bar.", "Baz.", "Quux.", "Plugh."}))
 		body = `He survives by stealing a parachute from the pilot, while Jaws lands on a trapeze net within a circus tent.
-		At the Drax Industries spaceplane-manufacturing complex in California, Bond meets the owner of the company, Hugo Drax, and his henchman Chang.`
-		Expect(split.SplitSentences(body)).To(Equal([]string{
+At the Drax Industries spaceplane-manufacturing complex in California, Bond meets the owner of the company, Hugo Drax, and his henchman Chang.`
+		Expect(split.Sentences(body)).To(Equal([]string{
 			"He survives by stealing a parachute from the pilot, while Jaws lands on a trapeze net within a circus tent.",
 			"At the Drax Industries spaceplane-manufacturing complex in California, Bond meets the owner of the company, Hugo Drax, and his henchman Chang.",
 		}))
 		body = `Contacting Leiter, the pair gets the U.S. Navy to intercept Disco Volante, to engage in an underwater battle, and to recover one of the bombs. Bond pursues Largo, and grabs hold of Disco Volante as she sheds the rear half to become a hydrofoil to escape.`
-		Expect(split.SplitSentences(body)).To(Equal([]string{
-			"Contacting Leiter, the pair gets the U.S.",
-			"Navy to intercept Disco Volante, to engage in an underwater battle, and to recover one of the bombs.",
+		Expect(split.Sentences(body)).To(Equal([]string{
+			"Contacting Leiter, the pair gets the U.S. Navy to intercept Disco Volante, to engage in an underwater battle, and to recover one of the bombs.",
 			"Bond pursues Largo, and grabs hold of Disco Volante as she sheds the rear half to become a hydrofoil to escape.",
+		}))
+		body = `While he is gone, Carver's assassin Dr. Kaufman kills Paris.`
+		Expect(split.Sentences(body)).To(Equal([]string{
+			`While he is gone, Carver's assassin Dr. Kaufman kills Paris.`,
 		}))
 	})
 })
