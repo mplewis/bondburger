39bdd45b2117703ba5a74a9b98c868cb869bf2b5
Matt Lewis <matt@mplewis.com>
2021-10-01 15:00:17 -0600

    feat: basic vite/vue app with router

---
+++ client/src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import "bulma/bulma.sass";
+</script>
+
+<template>
+  <div class="container p-3">
+    <router-view />
+  </div>
+</template>
---
+++ client/src/components/About.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+
+<template>
+  <p>This is the about page.</p>
+</template>
